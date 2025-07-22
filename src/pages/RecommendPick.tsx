8547a325c8748e1080fd1f3d036aff4a30c67304
7lram <105161457+7lram@users.noreply.github.com>
2025-07-22 16:21:02 +0900

    feat; topic pick page 연결

--- src/pages/RecommendPick.tsx
+++ src/pages/RecommendPick.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/common/Header';
 import Button from '../components/common/Button';
 
 const RecommendPick: React.FC = () => {
   const navigate = useNavigate();
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
-  const handleCategoryClick = (category: string) => {
-    console.log('선택된 카테고리:', category);
-    // 나중에 TopicRecommendPage로 이동하는 로직 추가
+
+  const handleCategoryClick =  (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  const handleSelectClick = async () => {
+    if (!selectedCategory) {
+      alert('카테고리를 선택해주세요!');
+      return;
+    }
+    try {
+      const response = await fetch(`/api/categoryDetails/${encodeURIComponent(selectedCategory)}`);
+      if (!response.ok) throw new Error('카테고리 예시 불러오기 실패');
+      const details = await response.json();
+      const examples = details.map((d: any) => d.content);
+
+      navigate('/topicdetail', {
+        state: {
+          topicName: selectedCategory,
+          examples,
+        }
+      });
+    } catch (err) {
+      alert('예시를 불러오지 못했습니다.');
+    }
   };
 
   return (
@@ -69,19 +92,31 @@ const RecommendPick: React.FC = () => {
           <div className="flex gap-[1.5vw] mb-[1.2vw] justify-center">
             <button
               onClick={() => handleCategoryClick("컨텐츠 공유형")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[16vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[16vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "컨텐츠 공유형"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">컨텐츠 공유형</span>
             </button>
             <button
               onClick={() => handleCategoryClick("너에 대해 더 알고 싶어")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[16vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[16vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "너에 대해 더 알고 싶어"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">너에 대해 더 알고 싶어</span>
             </button>
             <button
               onClick={() => handleCategoryClick("설렘이 필요할 때")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[16vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[16vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "설렘이 필요할 때"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">설렘이 필요할 때</span>
             </button>
@@ -91,19 +126,31 @@ const RecommendPick: React.FC = () => {
           <div className="flex gap-[1.5vw] mb-[1.2vw] justify-center">
             <button
               onClick={() => handleCategoryClick("취향 공유하기")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[16vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[16vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "취향 공유하기"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">취향 공유하기</span>
             </button>
             <button
               onClick={() => handleCategoryClick("과거 이야기")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[16vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[16vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "과거 이야기"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">과거 이야기</span>
             </button>
             <button
               onClick={() => handleCategoryClick("은근히 떠보기")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[16vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[16vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "은근히 떠보기"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">은근히 떠보기</span>
             </button>
@@ -113,25 +160,41 @@ const RecommendPick: React.FC = () => {
           <div className="flex gap-[1.5vw] mb-[1.2vw] justify-center">
             <button
               onClick={() => handleCategoryClick("만약에 시리즈")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[11.6vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[11.6vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "만약에 시리즈"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">만약에 시리즈</span>
             </button>
             <button
-              onClick={() => handleCategoryClick("만약에 시리즈")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[11.6vw] h-[6vh] flex items-center justify-center"
+              onClick={() => handleCategoryClick("연애 가치관")}
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[11.6vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "연애 가치관"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
-              <span className="text-[1.2vw] font-pretendard text-gray-700">만약에 시리즈</span>
+              <span className="text-[1.2vw] font-pretendard text-gray-700">연애 가치관</span>
             </button>
             <button
               onClick={() => handleCategoryClick("일상 공유형")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[11.6vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[11.6vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "일상 공유형"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">일상 공유형</span>
             </button>
             <button
               onClick={() => handleCategoryClick("이상형 토크")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[11.6vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[11.6vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "이상형 토크"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">이상형 토크</span>
             </button>
@@ -141,19 +204,31 @@ const RecommendPick: React.FC = () => {
           <div className="flex gap-[1.5vw] justify-center">
             <button
               onClick={() => handleCategoryClick("연애 밸런스 게임")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[16vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[16vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "연애 밸런스 게임"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">연애 밸런스 게임</span>
             </button>
-            <button
+           <button
               onClick={() => handleCategoryClick("티나는 호감 표현")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[16vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[16vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "티나는 호감 표현"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">티나는 호감 표현</span>
             </button>
             <button
               onClick={() => handleCategoryClick("TMI 폭격!")}
-              className="border border-gray-300 rounded-lg p-[1.5vw] text-center hover:border-custom-pink hover:bg-gray-50 transition-colors w-[16vw] h-[6vh] flex items-center justify-center"
+              className={`border rounded-lg p-[1.5vw] text-center transition-colors w-[16vw] h-[6vh] flex items-center justify-center
+                ${selectedCategory === "TMI 폭격!"
+                  ? "border-custom-pink bg-pink-50"
+                  : "border-gray-300 hover:border-custom-pink hover:bg-gray-50"}
+              `}
             >
               <span className="text-[1.2vw] font-pretendard text-gray-700">TMI 폭격!</span>
             </button>
@@ -165,7 +240,7 @@ const RecommendPick: React.FC = () => {
           <Button 
             variant="primary" 
             size="lg"
-            onClick={() => navigate('')}
+            onClick={handleSelectClick}
             className="w-full h-[8vh] text-[1.7vw] font-semibold"
           >
             선택하기
